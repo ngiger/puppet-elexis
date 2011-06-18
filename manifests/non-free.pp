8e7a3a75454d41579811a36003dc3c5952bdb19c
niklaus.giger@swissonline.ch <niklaus.giger@swissonline.ch>
2011-06-18 19:13:53 +0200

    Trie a few things

--- manifests/non-free.pp
+++ manifests/non-free.pp
@@ -8,18 +8,18 @@
  #   onlyif  => "match service-name[.='sshjump-$name'][port='$local_port'][protocol='tcp'] size == 0";
 
 define elexis::non-free($ensure, $type, $uri, $distribution = $lsbdistcodename, $component1, $component2 , $component3) {
-  augeas{"sources.list.$uri.$type":
-    context => "/files/etc/apt/sources.list",
-    changes => [
-       "set \"*[uri = '$uri' and type = '$type']/type\" $type",
-       "set \"*[uri = '$uri' and type = '$type']/uri\"  $uri",
-       "set \"*[uri = '$uri' and type = '$type']/distribution\" $distribution",
-       "set \"*[uri = '$uri' and type = '$type']/component[1]\" $component1",
-       "set \"*[uri = '$uri' and type = '$type']/component[2]\" $component2",
-       "set \"*[uri = '$uri' and type = '$type']/component[3]\" $component3",
-    ], 
-    onlyif => "match *[uri = '$uri' and type = '$type'] size > 0",
-  }
+#  augeas{"sources.list.$uri.$type":
+#    context => "/files/etc/apt/sources.list",
+#    changes => [
+#       "set \"*[uri = '$uri' and type = '$type']/type\" $type",
+#       "set \"*[uri = '$uri' and type = '$type']/uri\"  $uri",
+#       "set \"*[uri = '$uri' and type = '$type']/distribution\" $distribution",
+#       "set \"*[uri = '$uri' and type = '$type']/component[1]\" $component1",
+#       "set \"*[uri = '$uri' and type = '$type']/component[2]\" $component2",
+#       "set \"*[uri = '$uri' and type = '$type']/component[3]\" $component3",
+#    ], 
+#    onlyif => "match *[uri = '$uri' and type = '$type'] size > 0",
+#  }
 
 #  augeas{"sources.list.$uri.$type.add":
 #    context => "/files/etc/apt/sources.list",
@@ -38,20 +38,5 @@ define elexis::non-free($ensure, $type, $uri, $distribution = $lsbdistcodename, 
 #
 #  }
 
-      #"ins type uri component main",
-      #"set \"typ niklaus",
-      #"set uri  mirror",
-      #"set component non-freex",
-        #"set \"*[last()]/type\" $type",
-#        "set \"*[last()]/uri\" $uri",
-#        "set \"*[last()]/component[1]\" $component1",
-#        "set \"*[last()]/component[2]\" $component2",
-#        "set \"*[last()]/component[3]\" $component3",
-#        "set \"*[last()]/distribution\" $lsbdistcodename",
-#    "ins uri after uri[last()]",
-# was okay    "set \"1/type\" \"deb-ng\"",
-#	 "set \"/type[. = 'deb']\" \"deb-ngx\"",
-#    "set \'*[uri = 'my_mirror_path']/uri' my_mirror_path",
-#    "set \'*[uri = 'my_mirror_path']/distribution' squeeze",
 }
 
